f2ccb67e9df0d344e302a6f4f0802eec517b5d2d
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-08-23 15:11:21 -0500

    Functions 2

--- src/13-file.ts
+++ src/13-file.ts
@@ -1,11 +1,49 @@
 #!FUNCTIONS
 (() => {
+
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+
   function createProductToJson(
     title: string,
     createdAt: Date,
     stock: number,
-    size: number
+    size: Sizes
   ) {
-
+    return {
+      title,
+      createdAt,
+      stock,
+      size
+    }
   }
-})();
+
+  //const p1 = createProductToJson("Polo", new Date(), 12, "XL");
+  //console.log(p1);
+
+  const createProductToJsonV2 = (
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size?: Sizes
+  ) => {
+    return {
+      title,
+      createdAt,
+      stock,
+      size
+    }
+  }
+
+  const p2 = createProductToJsonV2("Polera Celeste", new Date(), 200);
+  console.log(p2);
+  console.log();
+  console.log(p2.title);
+  console.log(p2.stock);
+  console.log(p2.size);
+
+
+
+
+
+}
+)();
