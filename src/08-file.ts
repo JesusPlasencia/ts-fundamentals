b67e84c3104196b3d919f8cb31b6fd555f1dbd1f
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-08-23 11:35:49 -0500

    Arrays

---
+++ src/08-file.ts
@@ -0,0 +1,16 @@
+#!Arrays
+(() => {
+  let prices = [10, 15, 20, 25, 28, 30, 42, 'Hi', true];
+  prices.push(20);
+
+  let newArray = ['Hi', false];
+  newArray.push('Some content');
+
+  let mixed: (number | string | boolean | Object)[] = ['Hi', true];
+  mixed.push(200);
+  mixed.push({});
+
+  let numbers = [1, 2, 3, 4, 5, 6, 7];
+  numbers.map(item => item * 2);
+})();
+
