6cc76db357e68b1b607a58d6cad367133c3fcb97
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-08-23 10:35:58 -0500

    Numbers

---
+++ src/03-file.ts
@@ -0,0 +1,13 @@
+#!Typing
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 120;
+  //!Functions By Type (String)
+  myProductName = 'change';
+  myProductName.toLowerCase();
+  //!Functions By Type (Number)
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+  const myProductName2 = 'Product 1';
+})();
---
+++ src/05-file.ts
@@ -0,0 +1,27 @@
+#!Numbers
+(() => {
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('Product Price:', productPrice);
+
+  let customerAge: number = 28;
+  // customerAge = customerAge + '1'; 281
+  customerAge = customerAge + 1;
+  console.log('Customer Age:', customerAge);
+
+  let productInStock: number = 10;
+  console.log('Product In Stock:', productInStock);
+
+  let discount = parseInt('130');
+  console.log('Discount:', discount);
+
+  // #!Hexadecimal
+  let hex = 0xfff;
+  console.log('Hex:', hex);
+
+  // #!Bin
+  let bin = 0b10101;
+  console.log('Bin:', bin);
+
+  const nyNumber: Number = 10; //Se recomienda trabajar con Numbers
+})();
